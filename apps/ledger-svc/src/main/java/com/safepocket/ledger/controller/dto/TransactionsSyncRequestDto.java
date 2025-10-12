014274d65a8544727c57bc62c68c8fd4f4d2a4de
shota <shouta160618@gmail.com>
2025-10-11 22:53:44 -0500

    Add demoSeed flag to TransactionsSyncRequestDto record

--- apps/ledger-svc/src/main/java/com/safepocket/ledger/controller/dto/TransactionsSyncRequestDto.java
+++ apps/ledger-svc/src/main/java/com/safepocket/ledger/controller/dto/TransactionsSyncRequestDto.java
@@ -1,7 +1,11 @@
 package com.safepocket.ledger.controller.dto;
 
-public record TransactionsSyncRequestDto(String cursor, Boolean forceFullSync) {
+public record TransactionsSyncRequestDto(String cursor, Boolean forceFullSync, Boolean demoSeed) {
     public boolean forceFullSyncFlag() {
         return Boolean.TRUE.equals(forceFullSync);
+    }
+
+    public boolean demoSeedFlag() {
+        return Boolean.TRUE.equals(demoSeed);
     }
 }

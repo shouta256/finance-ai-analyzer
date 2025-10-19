ef9d8bbbc1827fbf88df25e09e0a23d80aafa6a1
shota <shouta160618@gmail.com>
2025-10-18 21:48:06 -0500

    Chore: remove app/page.tsx

--- apps/web/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Safepocket Home</h1>
-      <p>アプリは動作中です。/login または /dashboard へ移動してください。</p>
-    </main>
-  );
-}

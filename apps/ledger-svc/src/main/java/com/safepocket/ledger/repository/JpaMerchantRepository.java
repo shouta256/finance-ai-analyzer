5a101314ce88eb29279519dbd9e296c7b5162385
shota <shouta160618@gmail.com>
2025-11-05 17:50:29 -0600

    Update JpaMerchantRepository

--- apps/ledger-svc/src/main/java/com/safepocket/ledger/repository/JpaMerchantRepository.java
+++ apps/ledger-svc/src/main/java/com/safepocket/ledger/repository/JpaMerchantRepository.java
@@ -4,8 +4,11 @@ import com.safepocket.ledger.entity.MerchantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface JpaMerchantRepository extends JpaRepository<MerchantEntity, UUID> {
+
+    Optional<MerchantEntity> findByNameIgnoreCase(String name);
 }
